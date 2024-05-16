fc36ce54af17e5fc4b06543af636e0062aa23fd7
P0T4TO1 <jgarciag2005@alumno.ipn.mx>
2024-05-16 12:52:21 -0600

    update store orders page

--- src/components/orders/OrdersNegocio.tsx
+++ src/components/orders/OrdersNegocio.tsx
@@ -1,22 +1,16 @@
 "use client";
 
-import React, { useState, useContext, useEffect } from "react";
+import React from "react";
 
-import { CircularProgress } from "@nextui-org/react";
 import { OrdersTable } from "@/components";
 
-import { AuthContext } from "@/context/auth";
 import { IOrden } from "@/interfaces";
-import { hrApi } from "@/api";
 
 interface OrdersNegocioProps {
   orders: IOrden[];
 }
 
 export const OrdersNegocio = ({ orders }: OrdersNegocioProps) => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
   return (
     <div className="pt-12 container mx-auto min-h-screen">
       <div className="flex flex-col gap-4">
@@ -25,18 +19,7 @@ export const OrdersNegocio = ({ orders }: OrdersNegocioProps) => {
         </h1>
       </div>
 
-      {loading ? (
-        <div className="flex flex-col mt-12">
-          <CircularProgress size="lg" />
-          <p className="dark:text-gray-300">Cargando pedidos...</p>
-        </div>
-      ) : error ? (
-        <p>Hubo un error al cargar los pedidos</p>
-      ) : (
-        <>
-          <OrdersTable orders={orders} />
-        </>
-      )}
+      <OrdersTable orders={orders} />
     </div>
   );
 };
